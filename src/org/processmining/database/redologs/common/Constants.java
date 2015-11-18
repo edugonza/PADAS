df5e751e3fa0240adcc38f23a0122bda2b1c3e1e
Eduardo Gonzalez <edu.gonza.lopez@gmail.com>
2015-11-18 14:33:16 +0100

    Use of new POQL version with added language features

--- src/org/processmining/database/redologs/common/Constants.java
+++ src/org/processmining/database/redologs/common/Constants.java
@@ -1,7 +1,7 @@
 package org.processmining.database.redologs.common;
 
 public class Constants {
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.2.0";
 	public static final String AUTHOR = "Eduardo Gonzalez";
 	public static final String EMAIL = "e.gonzalez@tue.nl";
 }
